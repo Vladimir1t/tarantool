d15e366930d42f2138d99dcc6b0b53aacbddcf8d
Sergey Bronnikov <sergeyb@tarantool.org>
2024-08-05 14:07:41 +0300

    perf/lua: implement iterations in benchmark.lua [WIP]
    NO_CHANGELOG=perf
    NO_DOC=perf
    NO_TEST=perf

--- perf/lua/benchmark.lua
+++ perf/lua/benchmark.lua
@@ -72,7 +72,7 @@ local function dump_report(bench, text)
     end
 end
 
-local function add_result(bench, name, data)
+local function add_result(bench, name, data, i)
     local items_per_second = math.floor(data.items / data.real_time)
     local result = {
         name = name,
@@ -81,6 +81,8 @@ local function add_result(bench, name, data)
         iterations = data.items,
         items_per_second = items_per_second,
         run_name = name,
+        family_index = i,
+        per_family_instance_index = 0,
         run_type = 'iteration',
         repetitions = 1,
         repetition_index = 1,
